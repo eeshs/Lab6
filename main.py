640c45afd140722f192164cbbbd6a244cde8ff89
steffanocornejo <109877672+steffanocornejo@users.noreply.github.com>
2023-10-29 17:19:18 -0400

    New Changes!

--- main.py
+++ main.py
@@ -8,10 +8,31 @@ def encode(number):
             newchar = (int)(char) - 7
             newnum += (str)(newchar)
     return newnum
-#if __name__ == 'main':
-menu = 'Y'
-while menu != 'N':
-    number = (input)(print("Please enter a number you would like to encode"))
-    newnum = encode(number)
-    print("Your encoded number: " + newnum)
-    menu = (input)("Would you like to continue?(Y or N)")
+def decode(encoded_password):
+    num = 0
+    decoded_password = ' '
+    for char in encoded_password:
+        num = int(char) - 3
+        decoded_password += str(num)
+    return decoded_password
+def menu_display():
+    print("Menu \n ------------- \n 1. Encode \n 2. Decode \n 3. Quit\n")
+
+
+if __name__ == '__main__':
+    menu = 'Y'
+    newnum = 0
+    while menu != 'N':
+        menu_display()
+        choice = input("Enter a number")
+        if choice == "1":
+            number = (input("Please enter a number you would like to encode\n"))
+            newnum = encode(number)
+        elif choice == "2":
+            decoded = decode(newnum)
+            print(f"Your encoded password is {newnum} and the original password is {decoded}")
+        elif choice == "3":
+            break
+        print("Your encoded number: " + newnum)
+        menu = (input)("Would you like to continue?(Y or N)")
+
